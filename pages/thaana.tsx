77c80742060e3333ebb95960d02f97b967191b32
fayax555 <42441836+fayax555@users.noreply.github.com>
2021-12-24 12:30:58 +0500

    make thaana custom input

--- pages/thaana.tsx
+++ pages/thaana.tsx
@@ -1,5 +1,6 @@
-import { FC, useRef, useState, useEffect, ChangeEvent } from 'react'
+import { FC } from 'react'
 import styled from 'styled-components'
+import useThaana from 'utils/useThaanaInput'
 
 const Wrapper = styled.div`
   max-width: 900px;
@@ -22,109 +23,14 @@ const Wrapper = styled.div`
   }
 `
 
-const keyMap = {
-  q: 'ް',
-  w: 'އ',
-  e: 'ެ',
-  r: 'ރ',
-  t: 'ތ',
-  y: 'ޔ',
-  u: 'ު',
-  i: 'ި',
-  o: 'ޮ',
-  p: 'ޕ',
-  a: 'ަ',
-  s: 'ސ',
-  d: 'ދ',
-  f: 'ފ',
-  g: 'ގ',
-  h: 'ހ',
-  j: 'ޖ',
-  k: 'ކ',
-  l: 'ލ',
-  z: 'ޒ',
-  x: '×',
-  c: 'ޗ',
-  v: 'ވ',
-  b: 'ބ',
-  n: 'ނ',
-  m: 'މ',
-  Q: 'ޤ',
-  W: 'ޢ',
-  E: 'ޭ',
-  R: 'ޜ',
-  T: 'ޓ',
-  Y: 'ޠ',
-  U: 'ޫ',
-  I: 'ީ',
-  O: 'ޯ',
-  P: '÷',
-  A: 'ާ',
-  S: 'ށ',
-  D: 'ޑ',
-  F: 'ﷲ',
-  G: 'ޣ',
-  H: 'ޙ',
-  J: 'ޛ',
-  K: 'ޚ',
-  L: 'ޅ',
-  Z: 'ޡ',
-  X: 'ޘ',
-  C: 'ޝ',
-  V: 'ޥ',
-  B: 'ޞ',
-  N: 'ޏ',
-  M: 'ޟ',
-  ',': '،',
-  ';': '؛',
-  '?': '؟',
-  '<': '>',
-  '>': '<',
-  '[': ']',
-  ']': '[',
-  '(': ')',
-  ')': '(',
-  '{': '}',
-  '}': '{',
-} as { [key: string]: string }
-
 interface Props {}
 
 const Thaana: FC<Props> = () => {
-  const [text, setText] = useState('')
-  const [pos, setPos] = useState(0)
-
-  const inputRef = useRef<HTMLInputElement>(null)
-
-  useEffect(() => {
-    if (pos) {
-      // maintain cursor position
-      inputRef.current?.setSelectionRange(pos, pos)
-    }
-  }, [pos])
-
-  const props = {
-    ref: inputRef,
-    type: 'text',
-    value: text,
-    dir: 'rtl',
-  }
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const txt = e.target.value
-      .split('')
-      .map((char: string) => keyMap[char] || char)
-      .join('')
-
-    setText(txt)
-
-    const p = e.target.selectionStart
-    if (p) setPos(p)
-  }
+  const inputProps = useThaana()
 
   return (
     <Wrapper>
-      <input {...props} onChange={handleChange} />
+      <input {...inputProps} />
     </Wrapper>
   )
 }
---
+++ utils/useThaanaInput.ts
@@ -0,0 +1,97 @@
+import { ChangeEvent, useEffect, useRef, useState } from 'react'
+
+const keyMap = {
+  q: 'ް',
+  w: 'އ',
+  e: 'ެ',
+  r: 'ރ',
+  t: 'ތ',
+  y: 'ޔ',
+  u: 'ު',
+  i: 'ި',
+  o: 'ޮ',
+  p: 'ޕ',
+  a: 'ަ',
+  s: 'ސ',
+  d: 'ދ',
+  f: 'ފ',
+  g: 'ގ',
+  h: 'ހ',
+  j: 'ޖ',
+  k: 'ކ',
+  l: 'ލ',
+  z: 'ޒ',
+  x: '×',
+  c: 'ޗ',
+  v: 'ވ',
+  b: 'ބ',
+  n: 'ނ',
+  m: 'މ',
+  Q: 'ޤ',
+  W: 'ޢ',
+  E: 'ޭ',
+  R: 'ޜ',
+  T: 'ޓ',
+  Y: 'ޠ',
+  U: 'ޫ',
+  I: 'ީ',
+  O: 'ޯ',
+  P: '÷',
+  A: 'ާ',
+  S: 'ށ',
+  D: 'ޑ',
+  F: 'ﷲ',
+  G: 'ޣ',
+  H: 'ޙ',
+  J: 'ޛ',
+  K: 'ޚ',
+  L: 'ޅ',
+  Z: 'ޡ',
+  X: 'ޘ',
+  C: 'ޝ',
+  V: 'ޥ',
+  B: 'ޞ',
+  N: 'ޏ',
+  M: 'ޟ',
+  ',': '،',
+  ';': '؛',
+  '?': '؟',
+  '<': '>',
+  '>': '<',
+  '[': ']',
+  ']': '[',
+  '(': ')',
+  ')': '(',
+  '{': '}',
+  '}': '{',
+} as { [key: string]: string }
+
+const useThaanaInput = () => {
+  const [value, setText] = useState('')
+  const [pos, setPos] = useState(0)
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    if (pos) {
+      // maintain cursor position
+      inputRef.current?.setSelectionRange(pos, pos)
+    }
+  }, [pos])
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const txt = e.target.value
+      .split('')
+      .map((char: string) => keyMap[char] || char)
+      .join('')
+
+    setText(txt)
+
+    const p = e.target.selectionStart
+    if (p) setPos(p)
+  }
+
+  return { onChange, value, type: 'text', ref: inputRef }
+}
+
+export default useThaanaInput
